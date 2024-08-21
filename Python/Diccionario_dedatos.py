28b1ab7cc78c397fb646dc989597104ab679cc40
Jason Concepcion <jconcepcionb@hotmail.com>
2024-08-21 10:50:12 -0500

    Python

---
+++ Python/Diccionario_dedatos.py
@@ -0,0 +1,8 @@
+mi_diccionario = {
+    "persona1": {"nombre": "María", "edad": 30},
+    "persona2": {"nombre": "Jose", "edad": 30}
+}
+
+mi_diccionario["persona2"]  # Output: María
+
+print(mi_diccionario["persona1"])
